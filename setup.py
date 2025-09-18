dbf17e37795c73181578a818f030c533010a7d39
Byrja <byrja111@gmail.com>
2025-09-18 23:26:35 +0300

    Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="financial-telegram-bot",
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(),
     install_requires=[
         "python-telegram-bot==20.0",
